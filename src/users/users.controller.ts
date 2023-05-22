f4b42cdfc4bc04255c203c428cdfcc96f11040ef
aran <aranabdi@yahoo.com>
2023-05-22 09:35:40 +0330

    Connecting createUser to TypeORM repository and use service's create method in users.controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,12 +1,15 @@
 import { Body, Controller, Delete, Get, Patch, Post } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
+import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
 
+    constructor(private usersService: UsersService) {}
+
     @Post()
     createUser(@Body() body: CreateUserDto) {
-        console.log(body);
+        return this.usersService.create(body.email, body.password)
     }
 
     @Get()
