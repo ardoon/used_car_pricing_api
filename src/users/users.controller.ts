9bd3474d2f84f8d5fe17aa74194933a420d0615e
aran <aranabdi@yahoo.com>
2023-05-22 09:29:02 +0330

    Creating create-user.dto and applying it on createUser handler

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,10 +1,13 @@
-import { Controller, Delete, Get, Patch, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Patch, Post } from '@nestjs/common';
+import { CreateUserDto } from './dtos/create-user.dto';
 
 @Controller('users')
 export class UsersController {
 
     @Post()
-    createUser() {}
+    createUser(@Body() body: CreateUserDto) {
+        console.log(body);
+    }
 
     @Get()
     findUser() {}
