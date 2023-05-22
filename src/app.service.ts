2719d182c7732a0b7533cfabb4d2c62438b77167
aran <aranabdi@yahoo.com>
2023-05-22 08:45:14 +0330

    Root route of project rewriting

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'Welcome to "Used Car Pricing API" app, go to /auth for starting or check out request.http files in each users or reports directories for get informed about APIs.';
   }
 }
