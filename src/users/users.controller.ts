c87d222072b778309553573bc2f32df83926ef93
aran <aranabdi@yahoo.com>
2023-05-22 10:09:31 +0330

    Adding a Not Found Exception for findUser

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
+import { Body, Controller, Delete, Get, NotFoundException, Param, Patch, Post, Query } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UsersService } from './users.service';
 import { UpdateUserDto } from './dtos/update-user.dto';
@@ -14,8 +14,14 @@ export class UsersController {
     }
 
     @Get('/:id')
-    findUser(@Param('id') id: number) {
-        return this.usersService.findOne(id);
+    async findUser(@Param('id') id: number) {
+        const user = await this.usersService.findOne(id);
+
+        if(!user) {
+            throw new NotFoundException('User not found!')
+        }
+
+        return user;
     }
 
     @Get()
